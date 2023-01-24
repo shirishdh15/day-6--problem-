6b3d006a64fa38057325f94d328d3c0f03b01643
shirish <shirishdhalgade15@gmail.com>
2023-01-24 11:41:39 +0530

    UC2

---
+++ PerfectNumber.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int num = 28, sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == num) {
+            System.out.println(num + " is a perfect number");
+        } else {
+            System.out.println(num + " is not a perfect number");
+        }
+    }
+}
