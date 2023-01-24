1a4804ee1d458337f6f4de826fc69c7687931e51
shirish <shirishdhalgade15@gmail.com>
2023-01-24 11:45:20 +0530

    PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,21 @@
+package Day6;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 11, flag = 0;
+        for (int i = 2; i <= num/2; ++i) {
+            if (num % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (num == 1) {
+            System.out.println(num + " is not a prime number");
+        } else {
+            if (flag == 0)
+                System.out.println(num + " is a prime number.");
+            else
+                System.out.println(num + " is not a prime number.");
+        }
+    }
+}
